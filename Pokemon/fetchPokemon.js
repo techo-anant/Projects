ed8dff31af2a437b8ba161957d4439f1b0ad163e
Techo Anant <anantk.singh9876@gmail.com>
2024-12-22 12:10:32 -0500

    added submit on enter

--- Pokemon/fetchPokemon.js
+++ Pokemon/fetchPokemon.js
@@ -10,28 +10,39 @@
 //     .catch(error => console.error(error));
 
 
+const inputBox = document.querySelector(".search-input")
+const submitButton = document.querySelector(".submit");
+
+
+inputBox.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    if(event.key === "Enter"){
+        submitButton.click();
+    }
+});
+
+
 async function fetchdata() {
     try{
-
         const pokemonName = document.querySelector("#pokemonname").value.toLowerCase();
-
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         const notFound = document.querySelector("#wrongName");
         const imgElement = document.getElementById("pokemonSprite");
+        const pokeInfo = document.querySelector(".info");
         if(!response.ok){
             notFound.style.display = "block";
             imgElement.style.display = "none";
+            pokeInfo.style.display = "none";
             throw new Error("could not fetch resourse")
         }else{
             notFound.style.display = "none";
             const data = await response.json();
             console.log(data);
             const pokemonSprite = data.sprites.front_default;
-            const pokeInfo = document.querySelector(".info");
             const info = data.types;
 
             pokeInfo.innerHTML = `<h3>Type: ${info.map(ele => ele.type.name.charAt(0).toUpperCase()+ele.type.name.substring(1,)).join(", ")} </h3>`;
-
+            pokeInfo.style.display = "block";
             imgElement.src = pokemonSprite;
             imgElement.style.display = "block";
         }
